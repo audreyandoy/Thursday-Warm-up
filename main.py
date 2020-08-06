f0e830efb48ed15227ff6186dde5e516093e5e3a
Audrey Andoy <audreylandoy@gmail.com>
2020-08-06 19:38:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+# VARIABLES - stores a piece of data
+
+a = 2 #integer 
+b = "2" #string
+
+print(a+a)
+print(b+b)
+
+# What are the 2 characteristics of data?  VALUE and DATA TYPE
+
+# LOOP - repeats code for a given amount of times or until a condition is met. 
+
+# For loop 
+for i in range(5):
+  print("It's almost friday", i)
+
+# while
+# smelly = True
+# while smelly == True:
+#   print("i smell")
+#   smelly = False 
+counter = 0 
+while counter < 5:
+  print(counter)
+  counter += 1
+
+# Conditionals - IF/ELSE
+if counter == 0:
+  print("equals 0")
+elif counter > 5:
+  print("greater than 5")
+else:
+  print("less than 5")
+
+#1. Give me an example of a string variable and an integer variable
+num = 0
+name = "Audrey"
+#2. Create your OWN for loop
+for i in range(3):
+  print("I'm hungry")
+#3. Create your OWN while loop
+status = "hungry"
+while status == "hungry":
+  print("eat eat eat")
+  status = "full"
+  
+#4. In a comment, provide an example of when to use:
+     # a) a variable
+     # b) a loop
+     # c) a conditionals
+#5. Provide any questions that you currently have or provide a concept you're confused about.
+
